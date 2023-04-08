84ac5e4470be6578f069976a2fd00437e8dd40d1
Ricardo Chavez <rachav999iron@gmail.com>
2023-04-08 16:36:12 -0500

    DIBUJAR JUGADOR Y MOVIMIENTOS DENTRO DEL MAPA

--- game.js
+++ game.js
@@ -25,10 +25,16 @@ function resizeCanvas(){
 }
 
 let mapRowCols;
+let elementsSize;
+
+let positionJugador = {
+    x:undefined,
+    y:undefined
+}
 
 function starGame(){
     //Dividir el cuadro de canvas en 10
-    const elementsSize = canvasSize / 10;
+    elementsSize = canvasSize / 10;
 
     game.font = (elementsSize-12) + "px Verdana"; //siempre el tamaño con el tipo de letra. Resto 12 porque...para que no ocupe más de la cuenta.
     game.textAlign = "end"; //cada bombita acabara en la primera coordenada de fillText
@@ -38,16 +44,23 @@ function starGame(){
             game.fillText(emojis["X"],elementsSize*i+5,elementsSize*j-15);
         }
     } */
-    renderMap(2);
+    renderMap(0);
     //Imprimir emojis según según mapa
     for (let i = 1; i <= 10; i++) {
         for (let j = 1; j <= 10; j++) {
             game.fillText(emojis[mapRowCols[j-1][i-1]],elementsSize*i+5,elementsSize*j-15);
+            //Buscar la posición de la puerta y almacenar en el objeto de posicionJugador
+            if(mapRowCols[i-1][j-1]=="O"){
+                positionJugador.y = elementsSize*i, //filas
+                positionJugador.x = elementsSize*j  //columnas
+            }
         }
     }
+    movePlayer();
     //Métodos de canvas
     /* game.fillStyle = "rgb(0, 162, 232)";
     game.fillRect(0,0,50,50); */ //dibujuar un rectangulo (x,y,ancho,alto)
+
 }
 
 //Separar cada elemento del array map en un arreglo nuevo, primero en columnas y luego en filas
@@ -59,4 +72,63 @@ function renderMap(number){
     //}
 }
 
+//MOVIMIENTO BOTONES
+
+const btnUp = document.querySelector("#btnUp");
+const btnLeft = document.querySelector("#btnLeft");
+const btnDown = document.querySelector("#btnDown");
+const btnRight = document.querySelector("#btnRight");
+
+window.addEventListener("keydown",teclaSpecific);
+btnUp.addEventListener("click",teclaUp);
+btnLeft.addEventListener("click",teclaDown);
+btnDown.addEventListener("click",teclaLeft);
+btnRight.addEventListener("click",teclaRight);
+
+function teclaSpecific(e){
+    if(e.key=="ArrowUp") teclaUp();
+    else if(e.key=="ArrowDown") teclaDown();
+    else if(e.key=="ArrowLeft") teclaLeft();
+    else if(e.key=="ArrowRight") teclaRight();
+}
+
+function teclaUp(){
+    console.log("Te movista hacia arriba");
+    if(positionJugador.y > 100){ //evitar que se salga del mapa
+        positionJugador.y -= elementsSize;
+    }
+    movePlayer();
+}
+function teclaDown(){
+    console.log("Te movista hacia abajo");
+    if(positionJugador.y < canvasSize){
+        positionJugador.y += elementsSize;
+    }
+    movePlayer();
+}
+function teclaLeft(){
+    console.log("Te movista hacia izquierda");
+    if(positionJugador.x > 100){
+        positionJugador.x -= elementsSize;
+    }
+    movePlayer();
+}
+function teclaRight(){
+    console.log("Te movista hacia derecha");
+    if(positionJugador.x < canvasSize){
+        positionJugador.x += elementsSize;
+    }
+    movePlayer();
+}
+//Imprimir jugador cada vez que haya un cambio de posición
+function movePlayer(){
+    //Eliminar posición anterior jugador por cada movimiento.
+    game.clearRect(0,0,canvasSize,canvasSize);//borrar todo el contenido del canvas
+    for (let i = 1; i <= 10; i++) { //volver a renderizar el mapa con la nueva posición del jugador
+        for (let j = 1; j <= 10; j++) {
+            game.fillText(emojis[mapRowCols[j-1][i-1]],elementsSize*i+5,elementsSize*j-15);
+        }
+    }
+    game.fillText(emojis["PLAYER"],positionJugador.x+5,positionJugador.y-15);//imprimir al jugador
+}
 
